fef4ecc886e3a35d8caa0c92137880f4b3ff1868
Satoki Ogiso <ogiso@aclab.esys.tsukuba.ac.jp>
2015-01-16 01:42:05 +0900

    modified figure to be more readable

--- fig1.m
+++ fig1.m
@@ -21,6 +21,7 @@ dchirp2 = [dchirp, dchirp];%ドップラー効果計測のため2波にする
 
 signal_m = zeros(1,ceil(signal_length/dt));
 signal_b = zeros(1,ceil(signal_length/dt));
+stime = (0:floor(signal_length/dt))*dt;
 
 tstart_index = floor(tstart/dt); %送信開始時刻のインデックス
 signal_m(tstart_index:tstart_index + length(uchirp2)-1) = uchirp2;
@@ -28,16 +29,20 @@ signal_b(tstart_index:tstart_index + length(dchirp2)-1) = dchirp2;
 
 figure()
 subplot(511)
-plot(signal_m, 'r');
+plot(stime,signal_m, 'r');
+ylabel( {'transmitted', 'M-mode signal'}, 'FontName','Times','FontSize',16 );
 subplot(512)
-plot(signal_b);
+plot(stime,signal_b);
+ylabel( {'transmitted', 'B-mode signal'}, 'FontName','Times','FontSize',16 );
 subplot(513)
-plot(signal_b + signal_m);
+plot(stime,signal_b + signal_m);
+ylabel( {'simulated', ' received signal'}, 'FontName','Times','FontSize',16 ); 
 subplot(514)
 compressed_m = xcorr(signal_m+signal_b, uchirp);
-plot(compressed_m(length(signal_m):length(compressed_m)), 'r')%パルス圧縮結果の表示
+plot(stime,compressed_m(length(signal_m):length(compressed_m)), 'r')%パルス圧縮結果の表示
+ylabel( {'Compressed', ' M-mode signal'}, 'FontName','Times','FontSize',16 ); 
 subplot(515)
 compressed_b = xcorr(signal_m+signal_b, dchirp);
-plot(compressed_b(length(signal_b):length(compressed_b)))%パルス圧縮結果の表示
-
-
+plot(stime,compressed_b(length(signal_b):length(compressed_b)))%パルス圧縮結果の表示
+xlabel( 'Time', 'FontName','Times','FontSize',16 ); 
+ylabel( {'Compressed', ' B-mode signal'}, 'FontName','Times','FontSize',16 ); 
